2674753511c432ff1c7b522128712506d2d08b52
Mark <valyavka@ucu.edu.ua>
2020-02-13 15:39:33 +0200

    finished func to find closest

--- web_map_markiyan_valyavka.py
+++ web_map_markiyan_valyavka.py
@@ -1,8 +1,11 @@
 import folium
+import geopy
 
 # m = folium.Map(location=[45.5236, -122.6750])
 # m.save('index.html')
-
+# geolocator = geopy.Nominatim(user_agent="myGeolocator")
+# loc = geolocator.geocode("Las Vegas, Nevada, USA")
+# print(loc)
 
 def read_file():
     """ (None) -> (set)
@@ -42,4 +45,31 @@ def get_film_year_and_location(lines_set):
 
     return film_dict
 
-print(get_film_year_and_location(read_file()))
+def find_closest_films(year, location):
+    """ (int),(tuple) -> (set)
+
+    Function get a year of a film, user location, and filters 10 closest film.
+    """
+    film_dict = get_film_year_and_location(read_file())
+    closest_film_lst = []
+    geolocator = geopy.Nominatim(user_agent="myGeolocator", timeout=10)
+    x = 49.83826, 24.02324
+
+
+    if year in film_dict:
+        while film_dict[year] and len(closest_film_lst) < 50:
+            try:
+                film = film_dict[year].pop()
+                film_loc_obj = geolocator.geocode(film[1])
+                film_loc = film_loc_obj.latitude, film_loc_obj.longitude
+                closest_film_lst.append((film[0], film_loc))
+                # print(film[0],film_loc)
+            except:
+                continue
+
+    closest_film_lst.sort(key = lambda elm: abs(elm[1][0]-x[0])+abs(elm[1][1]-x[1]))
+    closest_film_set = set(closest_film_lst[0:10])
+
+    return closest_film_set
+print(find_closest_films(2000, 'a'))
+
